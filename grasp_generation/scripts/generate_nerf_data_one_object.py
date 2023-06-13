b4489f1a3b7d2f87803040e1c0e2270676c563b9
Tyler Lum <tylergwlum@gmail.com>
2023-06-13 16:15:10 -0700

    Add scripts to create nerfs for all objects

--- grasp_generation/scripts/generate_nerf_data_one_object.py
+++ grasp_generation/scripts/generate_nerf_data_one_object.py
@@ -9,32 +9,35 @@ import sys
 
 sys.path.append(os.path.realpath("."))
 
+from utils.object_model import ObjectModel
 from tap import Tap
 from tqdm import tqdm
 import subprocess
 
 
-class GenerateNerfDataArgumentParser(Tap):
+class GenerateNerfDataOneObjectArgumentParser(Tap):
     gpu: int = 0
     mesh_path: str = "../data/meshdata"
     output_nerf_path: str = "../data/nerfdata"
+    object_code: str = "sem-Xbox360-d0dff348985d4f8e65ca1b579a4b8d2"
 
 
-def main(args: GenerateNerfDataArgumentParser):
+def main(args: GenerateNerfDataOneObjectArgumentParser):
     # Check if script exists
-    script_to_run = "scripts/generate_nerf_data_one_object.py"
+    script_to_run = "scripts/generate_nerf_data_one_object_one_scale.py"
     assert os.path.exists(script_to_run)
 
-    object_codes = [
-        object_code
-        for object_code in os.listdir(args.mesh_path)
-        if os.path.isdir(os.path.join(args.mesh_path, object_code))
-    ]
-    for i, object_code in tqdm(
-        enumerate(object_codes),
-        desc="Generating NeRF data for all objects",
+    object_model = ObjectModel(
+        data_root_path=args.mesh_path,
+        batch_size_each=1,
+    )
+    object_scales = object_model.scale_choice.tolist()
+
+    for i, object_scale in tqdm(
+        enumerate(object_scales),
+        desc=f"Generating NeRF data for {args.object_code} at different scales",
         dynamic_ncols=True,
-        total=len(object_codes),
+        total=len(object_scales),
     ):
         command = " ".join(
             [
@@ -42,7 +45,8 @@ def main(args: GenerateNerfDataArgumentParser):
                 f"--gpu {args.gpu}",
                 f"--mesh_path {args.mesh_path}",
                 f"--output_nerf_path {args.output_nerf_path}",
-                f"--object_code {object_code}",
+                f"--object_code {args.object_code}",
+                f"--object_scale {object_scale}",
             ]
         )
         print(f"Running command {i}: {command}")
@@ -51,5 +55,5 @@ def main(args: GenerateNerfDataArgumentParser):
 
 
 if __name__ == "__main__":
-    args = GenerateNerfDataArgumentParser().parse_args()
+    args = GenerateNerfDataOneObjectArgumentParser().parse_args()
     main(args)
---
+++ grasp_generation/scripts/generate_nerf_data_one_object_one_scale.py
@@ -0,0 +1,61 @@
+"""
+Last modified date: 2023.06.13
+Author: Tyler Lum
+Description: Create NeRF Data in Isaac simulator
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.realpath("."))
+
+from utils.isaac_validator import IsaacValidator
+from utils.object_model import ObjectModel
+from utils.seed import set_seed
+from tap import Tap
+from tqdm import tqdm
+
+
+class GenerateNerfDataOneObjectOneScaleArgumentParser(Tap):
+    gpu: int = 0
+    mesh_path: str = "../data/meshdata"
+    output_nerf_path: str = "../data/nerfdata"
+    object_code: str = "sem-Xbox360-d0dff348985d4f8e65ca1b579a4b8d2"
+    object_scale: float = 0.1
+
+
+def main(args: GenerateNerfDataOneObjectOneScaleArgumentParser):
+    # TODO: Trying to do multiple scales in one python script caused segfaults for some reason: 872597665ff528720b46b4f0c0a95a4513c38f7c
+    # TODO: Currently assumes origin of urdf is ~same as center of mesh.bounds
+    set_seed(42)
+    os.environ.pop("CUDA_VISIBLE_DEVICES")
+
+    # Create sim
+    sim = IsaacValidator(
+        gpu=args.gpu,
+    )
+
+    # For each scale, create NeRF dataset
+    os.makedirs(args.output_nerf_path, exist_ok=True)
+    sim.set_obj_asset(
+        obj_root=os.path.join(args.mesh_path, args.object_code, "coacd"),
+        obj_file="coacd.urdf",
+    )
+    sim.add_env_nerf_data_collection(
+        obj_scale=args.object_scale,
+
+    )
+    output_nerf_object_path = os.path.join(
+        args.output_nerf_path, f"{args.object_code}_{args.object_scale:.2f}".replace(".", "_")
+    )
+    sim.save_images(folder=output_nerf_object_path)
+    sim.create_train_val_test_split(
+        folder=output_nerf_object_path, train_frac=0.8, val_frac=0.1
+    )
+    sim.reset_simulator()
+    sim.destroy()
+
+
+if __name__ == "__main__":
+    args = GenerateNerfDataOneObjectOneScaleArgumentParser().parse_args()
+    main(args)
