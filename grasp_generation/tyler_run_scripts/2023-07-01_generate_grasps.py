3a8363610b4a0da985e3092d1dd72c9772503493
Tyler Lum <tylergwlum@gmail.com>
2023-07-01 12:21:21 -0700

    Add tyler_run_scripts

---
+++ grasp_generation/tyler_run_scripts/2023-07-01_generate_grasps.py
@@ -0,0 +1,14 @@
+import subprocess
+
+command_parts = [
+    "CUDA_VISIBLE_DEVICES=0 python scripts/generate_grasps.py",
+    "--all",
+    "--seed 0",
+    "--result_path ../data/2023-07-01_debug_graspdata",
+    "--wandb_name debug",
+    "--visualization_freq 100",
+]
+
+full_command = " ".join(command_parts)
+print(f"Running command: {full_command}")
+# subprocess.run(full_command, shell=True, check=True)
