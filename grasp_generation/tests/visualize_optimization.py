b5bc60014395545987b6767b4bb361ef1df8b5c1
Tyler Lum <tylergwlum@gmail.com>
2023-07-01 15:47:22 -0700

    Add play button

--- grasp_generation/tests/visualize_optimization.py
+++ grasp_generation/tests/visualize_optimization.py
@@ -62,7 +62,7 @@ def main(args: VisualizeOptimizationArgumentParser):
     plotly_file_paths = download_plotly_files(run_path)
 
     # Read in json files
-    plotly_file_paths = plotly_file_paths[:args.max_files_to_read]
+    plotly_file_paths = plotly_file_paths[: args.max_files_to_read]
     orig_figs = [
         plotly.io.read_json(file=plotly_file_path)
         for plotly_file_path in plotly_file_paths
@@ -70,6 +70,7 @@ def main(args: VisualizeOptimizationArgumentParser):
 
     # Create new figure with all plots
     new_fig = go.Figure(
+        data=orig_figs[0].data,
         layout=go.Layout(
             scene=dict(
                 xaxis=dict(title="X"),
@@ -79,50 +80,72 @@ def main(args: VisualizeOptimizationArgumentParser):
             ),
             showlegend=True,
             title="new_fig",
-        )
-    )
-    fig_idx_per_trace = []
-    for fig_idx_to_visualize, fig in enumerate(orig_figs):
-        for d in fig.data:
-            new_fig.add_trace(d)
-            fig_idx_per_trace.append(fig_idx_to_visualize)
-
-    # Setup slider to show one figure per step
-    slider_steps = []
-    for fig_idx_to_visualize in range(len(orig_figs)):
-        # Only visualize the traces for this figure
-        visible_list = [
-            fig_idx_to_visualize == fig_idx for fig_idx in fig_idx_per_trace
-        ]
-
-        # Add a step to the slider for each figure
-        step = {
-            "method": "update",
-            "args": [{"visible": visible_list}],  # Layout attribute
-            "label": f"Plot {fig_idx_to_visualize + 1}",
-        }
-        slider_steps.append(step)
-
-    # Show one fig first
-    fig_to_show_first = 0
-    for i, fig_idx_to_visualize in enumerate(fig_idx_per_trace):
-        new_fig.data[i].visible = fig_idx_to_visualize == fig_to_show_first
-
-    new_fig.update_layout(
-        sliders=[
-            dict(
-                steps=slider_steps,
-                active=fig_to_show_first,  # Initial active index
-                currentvalue=dict(
-                    font=dict(size=12),
-                    prefix="Optimization Iter",  # Prefix for the slider label
-                    xanchor="center",
-                    visible=True,
+            updatemenus=[
+                dict(
+                    type="buttons",
+                    buttons=[dict(label="Play", method="animate", args=[None])],
+                )
+            ],
+        ),
+        frames=[
+            go.Frame(
+                data=fig.data,
+                layout=go.Layout(
+                    scene=dict(
+                        xaxis=dict(title="X"),
+                        yaxis=dict(title="Y"),
+                        zaxis=dict(title="Z"),
+                        aspectmode="data",
+                    ),
+                    showlegend=True,
+                    title=f"new_fig frame {i}",
                 ),
-                len=1.0,  # Length of the slider
             )
+            for i, fig in enumerate(orig_figs)
         ],
     )
+    #     fig_idx_per_trace = []
+    #     for fig_idx_to_visualize, fig in enumerate(orig_figs):
+    #         for d in fig.data:
+    #             new_fig.add_trace(d)
+    #             fig_idx_per_trace.append(fig_idx_to_visualize)
+    #
+    #     # Setup slider to show one figure per step
+    #     slider_steps = []
+    #     for fig_idx_to_visualize in range(len(orig_figs)):
+    #         # Only visualize the traces for this figure
+    #         visible_list = [
+    #             fig_idx_to_visualize == fig_idx for fig_idx in fig_idx_per_trace
+    #         ]
+    #
+    #         # Add a step to the slider for each figure
+    #         step = {
+    #             "method": "update",
+    #             "args": [{"visible": visible_list}],  # Layout attribute
+    #             "label": f"Plot {fig_idx_to_visualize + 1}",
+    #         }
+    #         slider_steps.append(step)
+    #
+    #     # Show one fig first
+    #     fig_to_show_first = 0
+    #     for i, fig_idx_to_visualize in enumerate(fig_idx_per_trace):
+    #         new_fig.data[i].visible = fig_idx_to_visualize == fig_to_show_first
+    #
+    #     new_fig.update_layout(
+    #         sliders=[
+    #             dict(
+    #                 steps=slider_steps,
+    #                 active=fig_to_show_first,  # Initial active index
+    #                 currentvalue=dict(
+    #                     font=dict(size=12),
+    #                     prefix="Optimization Iter",  # Prefix for the slider label
+    #                     xanchor="center",
+    #                     visible=True,
+    #                 ),
+    #                 len=1.0,  # Length of the slider
+    #             )
+    #         ],
+    #     )
 
     new_fig.show()
 
