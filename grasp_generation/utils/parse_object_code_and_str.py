48b74491a9fb04cd17df2adcffaeab3ee7cf3cc4
Tyler Lum <tylergwlum@gmail.com>
2023-08-24 23:55:46 -0700

    Shared code for parse_object_code_and_str

---
+++ grasp_generation/utils/parse_object_code_and_str.py
@@ -0,0 +1,10 @@
+def parse_object_code_and_scale(object_code_and_scale_str: str) -> Tuple[str, float]:
+    keyword = "_0_"
+    idx = object_code_and_scale_str.rfind(keyword)
+    object_code = object_code_and_scale_str[:idx]
+
+    idx_offset_for_scale = keyword.index("0")
+    object_scale = float(
+        object_code_and_scale_str[idx + idx_offset_for_scale :].replace("_", ".")
+    )
+    return object_code, object_scale
