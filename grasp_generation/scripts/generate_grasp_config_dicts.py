bb887df64772f860bc7d9c47d1bfa7bc01f7fb27
Tyler Lum <tylergwlum@gmail.com>
2023-08-24 21:06:32 -0700

    Fix up

--- grasp_generation/scripts/generate_grasp_config_dicts.py
+++ grasp_generation/scripts/generate_grasp_config_dicts.py
@@ -47,8 +47,10 @@ def split_object_code_and_scale(object_code_and_scale_str: str) -> Tuple[str, fl
     keyword = "_0_"
     idx = object_code_and_scale_str.rfind(keyword)
     object_code = object_code_and_scale_str[:idx]
+
+    idx_offset_for_scale = keyword.index('0')
     object_scale = float(
-        object_code_and_scale_str[idx + len(keyword) :].replace("_", ".")
+        object_code_and_scale_str[idx + idx_offset_for_scale :].replace("_", ".")
     )
     return object_code, object_scale
 
--- grasp_generation/tests/visualize_hand_config.py
+++ grasp_generation/tests/visualize_hand_config.py
@@ -11,76 +11,65 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from tap import Tap
-import torch
 import numpy as np
 import plotly.graph_objects as go
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Tuple
 
 from utils.hand_model import HandModel
 from utils.object_model import ObjectModel
 from utils.hand_model_type import handmodeltype_to_joint_names, HandModelType
 from utils.qpos_pose_conversion import qpos_to_pose
+import pathlib
 
 
-class VisualizeResultArgumentParser(Tap):
+class VisualizeHandConfigArgumentParser(Tap):
     hand_model_type: HandModelType = HandModelType.ALLEGRO_HAND
-    object_code: str = "sem-Xbox360-d0dff348985d4f8e65ca1b579a4b8d2"
+    input_hand_config_dicts_path: pathlib.Path = pathlib.Path(
+        "../data/hand_config_dicts"
+    )
+    object_code_and_scale_str: str = "sem-Ipod-4b6c6248d5c01b3e4eee8d1cb32988b_0_10"
     idx_to_visualize: int = 0
-    result_path: str = "../data/dataset"
     save_to_html: bool = False
 
 
-def get_contact_candidates_plot(link_name_to_contact_candidates: Dict[str, Any], plot_name: str, color: str) -> List[go.Scatter3d]:
-    all_contact_candidates = np.concatenate(
-        [
-            contact_candidates
-            for _, contact_candidates in link_name_to_contact_candidates.items()
-        ],
-        axis=0,
+def split_object_code_and_scale(object_code_and_scale_str: str) -> Tuple[str, float]:
+    keyword = "_0_"
+    idx = object_code_and_scale_str.rfind(keyword)
+    object_code = object_code_and_scale_str[:idx]
+
+    idx_offset_for_scale = keyword.index("0")
+    object_scale = float(
+        object_code_and_scale_str[idx + idx_offset_for_scale :].replace("_", ".")
     )
-    num_points = all_contact_candidates.shape[0]
-    assert all_contact_candidates.shape == (num_points, 3)
-    plotly_list = [
-        go.Scatter3d(
-            x=all_contact_candidates[:, 0],
-            y=all_contact_candidates[:, 1],
-            z=all_contact_candidates[:, 2],
-            mode="markers",
-            marker=dict(size=2, color=color),
-            name=plot_name,
-        )
-    ]
-    return plotly_list
+    return object_code, object_scale
 
 
-def main(args: VisualizeResultArgumentParser):
+def main(args: VisualizeHandConfigArgumentParser):
     device = "cpu"
 
     joint_names = handmodeltype_to_joint_names[args.hand_model_type]
+    object_code, object_scale = split_object_code_and_scale(
+        args.object_code_and_scale_str
+    )
 
     # load results
-    data_dict = np.load(
-        os.path.join(args.result_path, args.object_code + ".npy"), allow_pickle=True
-    )[args.idx_to_visualize]
+    data_dicts: List[Dict[str, Any]] = np.load(
+        args.input_hand_config_dicts_path / f"{args.object_code_and_scale_str}.npy",
+        allow_pickle=True,
+    )
+    data_dict = data_dicts[args.idx_to_visualize]
     hand_pose = qpos_to_pose(
         qpos=data_dict["qpos"], joint_names=joint_names, unsqueeze_batch_dim=True
     ).to(device)
-    hand_pose_st = (
+    hand_pose_start = (
         qpos_to_pose(
-            qpos=data_dict["qpos_st"], joint_names=joint_names, unsqueeze_batch_dim=True
+            qpos=data_dict["qpos_start"],
+            joint_names=joint_names,
+            unsqueeze_batch_dim=True,
         ).to(device)
-        if "qpos_st" in data_dict
+        if "qpos_start" in data_dict
         else None
     )
-
-    if "link_name_to_contact_candidates" in data_dict:
-        contact_candidates_plotly = get_contact_candidates_plot(link_name_to_contact_candidates=data_dict["link_name_to_contact_candidates"], plot_name="contact_candidates", color="red")
-    else:
-        contact_candidates_plotly = []
-    if "link_name_to_target_contact_candidates" in data_dict:
-        target_contact_candidates_plotly = get_contact_candidates_plot(link_name_to_contact_candidates=data_dict["link_name_to_target_contact_candidates"], plot_name="target_contact_candidates", color="green")
-    else:
-        target_contact_candidates_plotly = []
 
     # hand model
     hand_model = HandModel(hand_model_type=args.hand_model_type, device=device)
@@ -93,23 +82,23 @@ def main(args: VisualizeResultArgumentParser):
         num_samples=2000,
         device=device,
     )
-    object_model.initialize(args.object_code)
+    object_model.initialize(object_code)
 
     # visualize
-    if hand_pose_st is not None:
-        hand_model.set_parameters(hand_pose_st)
-        hand_st_plotly = hand_model.get_plotly_data(
+    if hand_pose_start is not None:
+        hand_model.set_parameters(hand_pose_start)
+        hand_start_plotly = hand_model.get_plotly_data(
             i=0, opacity=0.5, color="lightblue", with_contact_points=False
         )
     else:
-        hand_st_plotly = []
+        hand_start_plotly = []
 
     hand_model.set_parameters(hand_pose)
     hand_en_plotly = hand_model.get_plotly_data(
         i=0, opacity=1, color="lightblue", with_contact_points=False
     )
     object_plotly = object_model.get_plotly_data(i=0, color="lightgreen", opacity=1)
-    fig = go.Figure(hand_st_plotly + hand_en_plotly + object_plotly + contact_candidates_plotly + target_contact_candidates_plotly)
+    fig = go.Figure(hand_start_plotly + hand_en_plotly + object_plotly)
     if "energy" in data_dict:
         energy = data_dict["energy"]
         E_fc = round(data_dict["E_fc"], 3)
@@ -117,7 +106,9 @@ def main(args: VisualizeResultArgumentParser):
         E_pen = round(data_dict["E_pen"], 5)
         E_spen = round(data_dict["E_spen"], 5)
         E_joints = round(data_dict["E_joints"], 5)
-        result = f"Index {args.idx_to_visualize}  E_fc {E_fc}  E_dis {E_dis}  E_pen {E_pen}"
+        result = (
+            f"Index {args.idx_to_visualize}  E_fc {E_fc}  E_dis {E_dis}  E_pen {E_pen}"
+        )
         fig.add_annotation(text=result, x=0.5, y=0.1, xref="paper", yref="paper")
     fig.update_layout(scene_aspectmode="data")
 
@@ -126,7 +117,7 @@ def main(args: VisualizeResultArgumentParser):
         os.makedirs(output_folder, exist_ok=True)
         output_filepath = os.path.join(
             output_folder,
-            f"result_{args.object_code}-{args.idx_to_visualize}.html",
+            f"result_{args.object_code_and_scale_str}-{args.idx_to_visualize}.html",
         )
         print(f"Saving to {output_filepath}")
         fig.write_html(output_filepath)
@@ -134,6 +125,7 @@ def main(args: VisualizeResultArgumentParser):
         print("Showing figure...")
         fig.show()
 
+
 if __name__ == "__main__":
-    args = VisualizeResultArgumentParser().parse_args()
+    args = VisualizeHandConfigArgumentParser().parse_args()
     main(args)
