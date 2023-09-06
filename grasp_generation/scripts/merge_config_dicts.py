d0ae6c294ceb35f0e09bc4e95ec7460984ce685c
Preston Culbertson <pculbert@caltech.edu>
2023-09-05 19:32:30 -0700

    refactor merge script to use new data convention

--- grasp_generation/scripts/merge_config_dicts.py
+++ grasp_generation/scripts/merge_config_dicts.py
@@ -2,8 +2,11 @@ import numpy as np
 import pathlib
 from tap import Tap
 
+
 class MergeConfigDictsArgumentsParser(Tap):
-    input_config_dicts_path: pathlib.Path = pathlib.Path("../data/unmerged_config_dicts")
+    input_config_dicts_path: pathlib.Path = pathlib.Path(
+        "../data/unmerged_config_dicts"
+    )
     output_config_dicts_path: pathlib.Path = pathlib.Path("../data/config_dicts")
 
 
@@ -24,19 +27,31 @@ def main(args: MergeConfigDictsArgumentsParser) -> None:
         print(f"Found {len(filepaths_with_filename)} files with filename {filename}")
 
         num_grasps = 0
-        grasp_config_dicts_list = []
+        combined_grasp_config_dict = None
         for filepath in filepaths_with_filename:
-            grasp_config_dicts = np.load(filepath, allow_pickle=True)
-            num_grasps += len(grasp_config_dicts)
-            grasp_config_dicts_list.append(grasp_config_dicts)
+            if combined_grasp_config_dict is None:
+                combined_grasp_config_dict = np.load(filepath, allow_pickle=True).item()
+            else:
+                grasp_config_dict = np.load(filepath, allow_pickle=True).item()
+                num_grasps += grasp_config_dict["trans"].shape[0]
+                for key in combined_grasp_config_dict.keys():
+                    combined_grasp_config_dict[key] = np.concatenate(
+                        [
+                            combined_grasp_config_dict[key],
+                            grasp_config_dict[key],
+                        ],
+                        axis=0,
+                    )
 
-        new_grasp_config_dicts = np.concatenate(grasp_config_dicts_list, axis=0)
-        assert len(new_grasp_config_dicts) == num_grasps
+        # Shape checks.
+        for key in combined_grasp_config_dict.keys():
+            assert combined_grasp_config_dict[key].shape[0] == num_grasps
 
         new_filepath = args.output_config_dicts_path / filename
         print(f"Saving {num_grasps} grasps to {new_filepath}")
         print()
-        np.save(new_filepath, new_grasp_config_dicts, allow_pickle=True)
+        np.save(new_filepath, combined_grasp_config_dict, allow_pickle=True)
+
 
 if __name__ == "__main__":
     args = MergeConfigDictsArgumentsParser().parse_args()
