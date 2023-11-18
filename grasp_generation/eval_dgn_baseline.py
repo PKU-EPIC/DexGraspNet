a02c962e1283d063278f83898da63b424d180558
Tyler Lum <tylergwlum@gmail.com>
2023-11-17 21:33:09 -0800

    Clean up baseline code

--- grasp_generation/eval_dgn_baseline.py
+++ grasp_generation/eval_dgn_baseline.py
@@ -10,6 +10,9 @@ class ArgumentParser(Tap):
     nerf_meshdata_root_path: pathlib.Path = pathlib.Path("../data/nerf_meshdata")
     plan_using_nerf: bool = False
     eval_using_nerf: bool = False
+    output_eval_results_path: pathlib.Path = pathlib.Path(
+        "../data/eval_results"
+    ) / datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
 def print_and_run(command: str) -> None:
@@ -32,10 +35,7 @@ def main() -> None:
         args.nerf_meshdata_root_path.exists()
     ), f"{args.nerf_meshdata_root_path} does not exist"
 
-    output_eval_results_path = pathlib.Path(
-        "../data/eval_results"
-    ) / datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    output_eval_results_path.mkdir(parents=True, exist_ok=True)
+    args.output_eval_results_path.mkdir(parents=True)
 
     # NOTE: Expects the scale of the nerf_meshdata to be the same as the scale of the meshdata.
     planning_meshdata_root_path = (
@@ -52,7 +52,7 @@ def main() -> None:
     # Gen hand configs
     hand_gen_command = (
         f"python scripts/generate_hand_config_dicts.py --meshdata_root_path {planning_meshdata_root_path}"
-        + f" --output_hand_config_dicts_path {output_eval_results_path / 'hand_config_dicts'}"
+        + f" --output_hand_config_dicts_path {args.output_eval_results_path / 'hand_config_dicts'}"
         + " --use_penetration_energy"
     )
     print_and_run(hand_gen_command)
@@ -60,39 +60,33 @@ def main() -> None:
     # Gen grasp configs
     grasp_gen_command = (
         f"python scripts/generate_grasp_config_dicts.py --meshdata_root_path {planning_meshdata_root_path}"
-        + f" --input_hand_config_dicts_path {output_eval_results_path / 'hand_config_dicts'}"
-        + f" --output_grasp_config_dicts_path {output_eval_results_path / 'grasp_config_dicts'}"
+        + f" --input_hand_config_dicts_path {args.output_eval_results_path / 'hand_config_dicts'}"
+        + f" --output_grasp_config_dicts_path {args.output_eval_results_path / 'grasp_config_dicts'}"
     )
     print_and_run(grasp_gen_command)
 
     # Eval final grasp configs.
     eval_final_grasp_command = (
         "python scripts/eval_all_grasp_config_dicts.py"
-        + f" --input_grasp_config_dicts_path {output_eval_results_path / 'grasp_config_dicts'}"
-        + f" --output_evaled_grasp_config_dicts_path {output_eval_results_path / 'evaled_grasp_config_dicts'}"
+        + f" --input_grasp_config_dicts_path {args.output_eval_results_path / 'grasp_config_dicts'}"
+        + f" --output_evaled_grasp_config_dicts_path {args.output_eval_results_path / 'evaled_grasp_config_dicts'}"
         + f" --meshdata_root_path {eval_meshdata_root_path}"
     )
     print_and_run(eval_final_grasp_command)
 
-    # Look at success rate.
+    # Look at success rate of all grasps and best k grasps.
+    BEST_K = 10
     num_successes, num_total = 0, 0
-    for filepath in (output_eval_results_path / "evaled_grasp_config_dicts").iterdir():
-        print(f"Loading grasp config dicts from: {filepath}")
+    num_successes_best_k, num_total_best_k = 0, 0
+    for filepath in (
+        args.output_eval_results_path / "evaled_grasp_config_dicts"
+    ).iterdir():
         evaled_grasp_config_dict = np.load(filepath, allow_pickle=True).item()
         passed_eval = evaled_grasp_config_dict["passed_eval"]
         assert len(passed_eval.shape) == 1
 
         num_total += passed_eval.shape[0]
         num_successes += passed_eval.sum()
-    print(f"Total success rate: {num_successes / num_total}")
-
-    BEST_K = 10
-    num_successes_best_k, num_total_best_k = 0, 0
-    for filepath in (output_eval_results_path / "evaled_grasp_config_dicts").iterdir():
-        print(f"Loading grasp config dicts from: {filepath}")
-        evaled_grasp_config_dict = np.load(filepath, allow_pickle=True).item()
-        passed_eval = evaled_grasp_config_dict["passed_eval"]
-        assert len(passed_eval.shape) == 1
 
         total_energy = evaled_grasp_config_dict["Total Energy"]
         assert total_energy.shape == passed_eval.shape
@@ -103,7 +97,12 @@ def main() -> None:
 
         num_total_best_k += passed_eval[best_k_indices].shape[0]
         num_successes_best_k += passed_eval[best_k_indices].sum()
-    print(f"Total success rate of top {BEST_K} energy: {num_successes_best_k / num_total_best_k}")
+    print(
+        f"Total success rate: num_successes / num_total = {num_successes} / {num_total} = {num_successes / num_total}"
+    )
+    print(
+        f"Total success rate of top {BEST_K} energy: num_successes_best_k / num_total_best_k = {num_successes_best_k} / {num_total_best_k} = {num_successes_best_k / num_total_best_k}"
+    )
 
 
 if __name__ == "__main__":
