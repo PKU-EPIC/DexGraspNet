bb317b71f09aecbb1abf0e183a5b7c8eda9a7b35
Tyler Lum <tylergwlum@gmail.com>
2024-01-24 18:07:27 -0800

    Improve prints

--- grasp_generation/scripts/eval_grasp_config_dict.py
+++ grasp_generation/scripts/eval_grasp_config_dict.py
@@ -327,6 +327,11 @@ def main(args: EvalGraspConfigDictArgumentParser):
     else:
         passed_penetration_threshold_array = E_pen_array < args.penetration_threshold
 
+    passed_eval = (
+        passed_simulation_array
+        * passed_penetration_threshold_array
+        * passed_new_penetration_test_array
+    )
     # TODO: Remove these prints
     DEBUG = True
     if DEBUG:
@@ -340,12 +345,8 @@ def main(args: EvalGraspConfigDictArgumentParser):
             f"passed_penetration_threshold_array = {passed_penetration_threshold_array} ({passed_penetration_threshold_array.mean() * 100:.2f}%)"
         )
         print(f"E_pen_array = {E_pen_array}")
+        print(f"passed_eval = {passed_eval}")
 
-    passed_eval = (
-        passed_simulation_array
-        * passed_penetration_threshold_array
-        * passed_new_penetration_test_array
-    )
     sim_frac = np.mean(passed_simulation_array)
     new_pen_frac = np.mean(passed_new_penetration_test_array)
     pen_frac = np.mean(passed_penetration_threshold_array)
