01c61ddb0b04c1ab323815f221e7bf96ab0a3248
Tyler Lum <tylergwlum@gmail.com>
2024-03-01 02:47:33 -0800

    Add TYLER_SCRATCH_ROT

--- grasp_generation/TYLER_SCRATCH_2_rot.py
+++ grasp_generation/TYLER_SCRATCH_2_rot.py
@@ -29,29 +29,37 @@ data = {k: v[IDX] for k, v in data_dict.items()}
 
 # %%
 N = 100
-x_noise = np.linspace(-0.1, 0.1, N)
-y_noise = np.linspace(-0.1, 0.1, N)
-xy_noise = np.array(np.meshgrid(x_noise, y_noise)).T.reshape(-1, 2)
-print(f"xy_noise.shape = {xy_noise.shape}")
+r_noise = np.linspace(-30, 30, N)
+p_noise = np.linspace(-30, 30, N)
+rp_noise = np.array(np.meshgrid(r_noise, p_noise)).T.reshape(-1, 2)
+print(f"rp_noise.shape = {rp_noise.shape}")
 
 # %%
 import matplotlib.pyplot as plt
-plt.scatter(xy_noise[:, 0], xy_noise[:, 1], s=0.1)
+plt.scatter(rp_noise[:, 0], rp_noise[:, 1], s=0.1)
 
 # %%
-data['trans']
+data['rot']
 
 # %%
 new_data_dict = {k: v[None, ...].repeat(N**2, axis=0) for k, v in data.items()}
-new_data_dict["trans"][..., :2] += xy_noise
+
+from scipy.spatial.transform import Rotation as R
+
+rot = new_data_dict["rot"][0]
+assert rot.shape == (3, 3)
+rpy = R.from_matrix(rot).as_euler('xyz', degrees=True)
+new_rpy = rpy[None, ...].repeat(N**2, axis=0)
+new_rpy[..., :2] += rp_noise
+new_data_dict["rot"][:] = R.from_euler('xyz', new_rpy, degrees=True).as_matrix()
 
 # %%
-plt.scatter(new_data_dict["trans"][:, 0], new_data_dict["trans"][:, 1], s=0.1)
-
+new_rpy_2 = R.from_matrix(new_data_dict["rot"]).as_euler('xyz', degrees=True)
+plt.scatter(new_rpy_2[:, 0], new_rpy_2[:, 1], s=0.1)
 
 # %%
-# new_data_path = pathlib.Path(f"../data/PROBE_{IDX}_2024-02-07_softball_0-051_5random/grasp_config_dicts/ddg-ycb_054_softball_0_0510.npy")
-new_data_path = pathlib.Path(f"../data/PROBE_{IDX}_2024-02-07_50mugs_0-075_5random/grasp_config_dicts_train/core-mug-10f6e09036350e92b3f21f1137c3c347_0_0750.npy")
+# new_data_path = pathlib.Path(f"../data/ROT_PROBE_{IDX}_2024-02-07_softball_0-051_5random/grasp_config_dicts/ddg-ycb_054_softball_0_0510.npy")
+new_data_path = pathlib.Path(f"../data/ROT_PROBE_{IDX}_2024-02-07_50mugs_0-075_5random/grasp_config_dicts_train/core-mug-10f6e09036350e92b3f21f1137c3c347_0_0750.npy")
 
 new_data_path.parent.mkdir(parents=True, exist_ok=True)
 np.save(new_data_path, new_data_dict)
--- grasp_generation/TYLER_SCRATCH_3.py
+++ grasp_generation/TYLER_SCRATCH_3.py
@@ -2,7 +2,8 @@
 import numpy as np
 import pathlib
 
-data_path = pathlib.Path("../data/PROBE_0_2024-02-07_softball_0-051_5random/evaled_grasp_config_dicts/ddg-ycb_054_softball_0_0510.npy")
+data_path = pathlib.Path("../data/PROBE_2024-02-07_softball_0-051_5random/evaled_grasp_config_dicts/ddg-ycb_054_softball_0_0510.npy")
+# data_path = pathlib.Path(f"../data/PROBE_7_2024-02-07_50mugs_0-075_5random/evaled_grasp_config_dicts_train/core-mug-10f6e09036350e92b3f21f1137c3c347_0_0750.npy")
 assert data_path.exists()
 
 # %%
@@ -23,7 +24,7 @@ plt.hist(passed_sim)
 
 # %%
 # Color each point by whether it passed the simulation.
-plt.scatter(data_dict["trans"][:, 0], data_dict["trans"][:, 1], s=0.1, c=passed_sim)
+plt.scatter(data_dict["trans"][:, 0], data_dict["trans"][:, 1], s=1, c=passed_sim)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Passed simulation')
